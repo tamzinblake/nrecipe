8931d7b6f1e061cbcaab1a6e5244f3b8bcef0c56
Thom Blake <webmaster@thomblake.com>
2011-03-17 15:30:12 +0000

    cromulated addComboBox.js

--- nrecipe/htdocs/js/addComboBox.js
+++ nrecipe/htdocs/js/addComboBox.js
@@ -1,41 +1,37 @@
-AddComboBox = Ext.extend(Ext.form.ComboBox, {
-    onLoad : function(){
-	if(!this.hasFocus){
-	    return;
-	}
-	if(this.store.getCount() > 0){
-	    this.expand();
-	    this.restrictHeight();
-	    if(this.lastQuery == this.allQuery){
-		if(this.editable){
-		    this.el.dom.select();
-		}
-		//if(!this.selectByValue(this.value, true)){
-		//	this.select(0, true);
-		//}
-	    }else{
-		//this.selectNext();
-		if(this.typeAhead &&
-		   this.lastKey != Ext.EventObject.BACKSPACE &&
-		   this.lastKey != Ext.EventObject.DELETE){
-		    this.taTask.delay(this.typeAheadDelay);
-		}
-	    }
-	}else{
-	    this.onEmptyResults();
-	}
-    },
-    onViewClick : function(doFocus){
-	var index = this.view.getSelectedIndexes()[0];
-	var r = this.store.getAt(index);
-	if(r){
-	    this.onSelect(r, index);
-	} else {
-	    this.collapse();
-	}
-	if(doFocus !== false){
-	    this.el.focus();
-	}
+AddComboBox = Ext.extend(Ext.form.ComboBox,
+  { onLoad : function () {
+      if (!this.hasFocus) {
+        return
+      }
+      if (this.store.getCount() > 0) {
+        this.expand()
+        this.restrictHeight()
+        if (this.lastQuery == this.allQuery) {
+          if (this.editable) {
+            this.el.dom.select()
+          }
+          if ( this.typeAhead
+            && this.lastKey != Ext.EventObject.BACKSPACE
+            && this.lastKey != Ext.EventObject.DELETE) {
+              this.taTask.delay(this.typeAheadDelay)
+            }
+        }
+      }
+      else {
+        this.onEmptyResults()
+      }
     }
-});
-
+  , onViewClick : function(doFocus) {
+      var index = this.view.getSelectedIndexes()[0]
+      var r = this.store.getAt(index)
+      if (r) {
+        this.onSelect(r, index)
+      }
+      else {
+        this.collapse()
+      }
+      if(doFocus !== false) {
+        this.el.focus()
+      }
+    }
+  } )
